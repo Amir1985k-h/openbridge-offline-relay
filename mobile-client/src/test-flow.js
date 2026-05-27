de08714b1b355c53201f514ecfbf5e73790bc7e5
Amir1985k-h <saghi8499@gmail.com>
2026-05-27 10:08:45 +0330

    Update test-flow.js

--- mobile-client/src/test-flow.js
+++ mobile-client/src/test-flow.js
@@ -1,18 +1,6 @@
 // mobile-client/src/test-flow.js
-const { signTransaction } = require('./screens/SignTransactionScreen');
+const { startSigningUI } = require('./screens/SignTransactionScreen');
 
-async function runTest() {
-    console.log("\n🚀 تست صفحه امضا\n");
+console.log("🚀 OpenBridge Mobile Signer Started\n");
 
-    const privateKey = "0xb1319cbc81f2c10ed272a6cefe5a823a22a7b9d0b24519be4deb60f9844e2daf"; // کلید تست
-    const toAddress = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e";
-    const amount = "0.0001";
-
-    const result = await signTransaction(privateKey, toAddress, amount);
-    
-    if (result.success) {
-        console.log("\n✅ تست نهایی با موفقیت تمام شد!");
-    }
-}
-
-runTest();
+startSigningUI();
